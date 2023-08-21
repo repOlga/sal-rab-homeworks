2ee1ce0f49daea643591422ea31816c0396abd09
repOLGA <omchirkova@mail.com>
2023-08-21 22:01:07 +0700

    task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -15,6 +15,17 @@ function addInCartMessage(title, price) {
     // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
+    let message = productName;
+    message += " ";
+    message += "за";
+    message += " ";
+    message += productPrice;
+    message += " ";
+    message += "теперь в корзине!";
+   
+    //let message = `${productName} за ${productPrice} тепреь в корзине!`;
+    console.log(message);
+
 
     return message;
 }
@@ -29,6 +40,7 @@ function addInCartCountChange(value) {
     // увеличьте значение newValue на 1
 
     // Конец решения задания №1.2.
+    let newValue = oldValue + 1;
 
     return newValue;
 }
@@ -44,6 +56,8 @@ function addInCartSumChange(sum, delta) {
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
     // Конец решения задания №1.3.
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     return newSumText;
 }
