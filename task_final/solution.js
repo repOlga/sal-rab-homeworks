54cd5a17c8117e6b1313a3ac03127d6eba189e6c
repOLGA <omchirkova@mail.com>
2023-09-07 01:26:51 +0700

    commit finaly task

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+ 
+    let data = {client:"", goods: [], order: {address, sum}};
+    
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    
+    //"ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53"
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    data.client = `${name} ${phone}`;
+   
+    //let jsonData = JSON.stringify(data);//
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
